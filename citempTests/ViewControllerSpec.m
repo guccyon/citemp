7f2a22a74fe2fefb42b62efd335961eccfa1f23d
Tetsuro Higuchi <tetsu@ageet.com>
2014-02-27 13:00:08 +0800

    added Dummy test case for Kiwi.

---
+++ citempTests/ViewControllerSpec.m
@@ -0,0 +1,21 @@
+//
+//  ViewControllerSpec.m
+//  citemp
+//
+//  Created by tetsu@ageet.com on 2014/02/27.
+//  Copyright 2014年 tetsu@ageet.com. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "ViewController.h"
+
+
+SPEC_BEGIN(ViewControllerSpec)
+
+describe(@"ViewController", ^{
+    it(@"", ^{
+        [[theValue(2) should] equal:theValue(3)];
+    });
+});
+
+SPEC_END
